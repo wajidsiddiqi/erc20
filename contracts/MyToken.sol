dc16042c556df46b67b9d55e9696cbba4a702bdc
wajid <wajidsiddiqi12@gmail.com>
2023-11-26 12:02:30 +0500

    added errors

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
+error MyToken__ReachedMaxSupply();
+error MyToken__ExceedingMaxHolding();
 
 contract MyToken is ERC20, Ownable {
     //* State Variables
